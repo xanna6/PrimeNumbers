9bd9f36f750cd15813975d567b5074e6fcf46715
xanna6 <apiotrow96@gmail.com>
2023-07-29 20:16:10 +0200

    added user interface

---
+++ src/main/java/apiotrowska/primenumbers/controller/HomeController.java
@@ -0,0 +1,15 @@
+package apiotrowska.primenumbers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "/calculate")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}

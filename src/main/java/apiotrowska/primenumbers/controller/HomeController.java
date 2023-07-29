88a4684a29584439c3e8b6f3ffd918037a88424d
xanna6 <apiotrow96@gmail.com>
2023-07-29 20:38:31 +0200

    sending entered boundary by post request

--- src/main/java/apiotrowska/primenumbers/controller/HomeController.java
+++ src/main/java/apiotrowska/primenumbers/controller/HomeController.java
@@ -1,15 +1,25 @@
 package apiotrowska.primenumbers.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping( "/calculate")
 public class HomeController {
 
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("boundary", null);
+        return "home";
+    }
+
+    @PostMapping
+    public String findPrimeNumbers(@RequestParam("boundary") Integer upperBoundary) {
+        System.out.println(upperBoundary);
         return "home";
     }
 }
